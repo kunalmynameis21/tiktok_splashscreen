3c39e9f5e20d00ea29ed1580a085c91bc4175778
Kunal Kumar Sharma <kunalmynameis21@gmail.com>
2025-06-04 10:41:42 +0530

    flutterwork
    allmyworl

--- new_project/android/app/src/main/kotlin/com/example/new_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.new_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- untitledu9/android/app/src/main/kotlin/com/example/untitledu9/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.untitledu9
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
